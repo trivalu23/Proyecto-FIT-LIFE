54901f92cf847c656d04ebbb3f222d4e6901e274
trivalu23 <niccondooriasto@crackthecode.la>
2025-09-08 21:28:15 -0500

    Rutas corregidas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 function calculateIMC() {
-    var weight = parseFloat(document.getElementById('weight').value);
-    var height = parseFloat(document.getElementById('height').value);
+  var weight = parseFloat(document.getElementById("weight").value);
+  var height = parseFloat(document.getElementById("height").value);
 
-    if (!isNaN(weight) && !isNaN(height)) {
-        var bmi = weight / (height ** 2);
-        var result = document.getElementById('result');
+  if (!isNaN(weight) && !isNaN(height)) {
+    var bmi = weight / height ** 2;
+    var result = document.getElementById("result");
 
+    result.innerHTML = "Tu IMC es:" + bmi.toFixed(2);
 
+    // Clasificación del IMC
 
-result.innerHTML = 'Tu IMC es:' + bmi.toFixed(2);
-
-// Clasificación del IMC
-
-
-        if (bmi < 18.5) {
-            result.innerHTML += ' - Bajo peso';
-        } else if (bmi < 25) {
-            result.innerHTML += ' - Peso normal';
-        } else if (bmi < 30) {
-            result.innerHTML += ' - Sobrepeso';
-        } else {
-            result.innerHTML += ' - Obesidad';
-        }
+    if (bmi < 18.5) {
+      result.innerHTML += " - Bajo peso";
+    } else if (bmi < 25) {
+      result.innerHTML += " - Peso normal";
+    } else if (bmi < 30) {
+      result.innerHTML += " - Sobrepeso";
+    } else {
+      result.innerHTML += " - Obesidad";
     }
+  }
 }
