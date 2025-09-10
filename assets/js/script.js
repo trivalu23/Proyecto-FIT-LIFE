e0c3ab1bd2b18f3fe7b5707d2b483816151bcbd7
trivalu23 <niccondooriasto@crackthecode.la>
2025-09-10 14:03:23 -0500

    Eliminé el script de servicios

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,3 @@ function calculateIMC() {
     }
   }
 }
-
-// DOM: obtener el botón
-const button = document.getElementById("goToPage");
-
-// Escuchar el clic y redirigir a otra página
-button.addEventListener("click", () => {
-  window.location.href = "./assets/html/formulario-servicios.html";
-});
