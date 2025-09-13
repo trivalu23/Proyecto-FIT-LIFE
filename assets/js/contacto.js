e775391b31f89dc0e6611ac5615988af8b065501
luisa-web <luipayanomendez@crackthecode.la>
2025-09-12 21:28:59 -0500

    fix: corrected code

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -16,14 +16,6 @@ document
 
     let errores = false;
 
-    if (username === "" || username.length < 3) {
-      mostrarError(
-        "username",
-        "⚠️ El nombre debe tener al menos 3 caracteres."
-      );
-      errores = true;
-    }
-
     if (mensage === "" || mensage.length < 10) {
       mostrarError(
         "mensage",
@@ -38,20 +30,20 @@ document
     document.getElementById("formContacto").reset();
   });
 
-// Función para mostrar errores debajo del campo
+// Mostrar errores debajo del campo
 function mostrarError(idCampo, mensaje) {
-  var campo = document.getElementById(idCampo);
-  var mensajeError = document.createElement("p");
+  const campo = document.getElementById(idCampo);
+  const mensajeError = document.createElement("p");
   mensajeError.innerText = mensaje;
   mensajeError.style.color = "red";
   mensajeError.classList.add("mensaje-error");
   campo.parentNode.appendChild(mensajeError);
 }
 
-// Función para limpiar mensajes antes de validar
+// Limpiar mensajes antes de validar
 function limpiarMensajes() {
-  var mensajes = document.querySelectorAll(".mensaje-error");
-  for (var i = 0; i < mensajes.length; i++) {
+  const mensajes = document.querySelectorAll(".mensaje-error");
+  for (let i = 0; i < mensajes.length; i++) {
     mensajes[i].remove();
   }
 }
